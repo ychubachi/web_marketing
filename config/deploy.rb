fdb400a8590b3edf786f7e07ddcf0f9e6c516edd
Yoshihide Chubachi <yoshi@chubachi.net>
2012-06-22 00:12:15 +0900

    database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,6 @@ role :web, "pr.aiit.ac.jp"
 role :app, "pr.aiit.ac.jp"
 role :db,  "pr.aiit.ac.jp", :primary => true
 
-If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
