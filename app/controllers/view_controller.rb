dd7e7f0ee189165e18c6a1f2cfcb11f9267ee302
Yoshihide Chubachi <yoshi@chubachi.net>
2012-06-24 15:40:46 +0900

    remove view_controller, move things to home_controller from redirection_controller

--- app/controllers/view_controller.rb
+++
@@ -1,4 +0,0 @@
-class ViewController < ApplicationController
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 WebMarketing::Application.routes.draw do
   resources :pages do as_routes end
-
   resources :pages
-
   resources :requests do as_routes end
   resources :requests
   resources :actions do as_routes end
@@ -18,19 +16,16 @@ WebMarketing::Application.routes.draw do
   resources :browsers do as_routes end
   resources :browsers
 
-  get "redirection/index"
-  get "redirection/redirect"
-  match '/rd/:code' => 'redirection#redirect'
-  root to: "redirection#index"
-
-  get "view/index"
-
-  #
   get 'home/index'
-  authenticated :user do
-    root :to => 'home#index'
-  end
-  # root :to => "home#index"
+  get "home/redirect"
+  get 'home/admin'
+  match '/rd/:code' => 'home#redirect'
+  match '/admin/'   => 'home#admin'
+  root to: "home#index"
+
+#  authenticated :user do
+#    root :to => 'home#admin'
+#  end
   devise_for :users
   resources :users, :only => [:show, :index]
 end
