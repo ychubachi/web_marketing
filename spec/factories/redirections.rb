0e9160bb4d3572dc2dec56f8663a93fab1e3f0f2
Yoshihide Chubachi <yoshi@chubachi.net>
2012-06-24 18:22:12 +0900

    delete unnecesary specs

--- spec/factories/redirections.rb
+++ spec/factories/redirections.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :redirect do
+  factory :redirection do
     code "MyString"
     default_redirect false
     medium_id 1
