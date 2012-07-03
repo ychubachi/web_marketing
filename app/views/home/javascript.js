7665d2856f27782a29599789bc63b23f80ef8491
Yoshihide Chubachi <yoshi@chubachi.net>
2012-07-03 11:07:29 +0900

    send uuid via params

--- app/views/home/javascript.js
+++ app/views/home/javascript.js
@@ -1,13 +1,27 @@
 function sendPostRequest() {
-    url = 'http://pr.aiit.ac.jp/pv';
-    console.log('url : ' + url)
+    console.log('cookie : ' + document.cookie);
+
+    uuid = "";
+    key = "web_marketing_uuid=";
+    i = 0;
+    cookie_array = document.cookie.split("; ");
+    while(cookie_array[i]) {
+	console.log(cookie_array[i]);
+	if(cookie_array[i].substr(0, key.length) == key) {
+	    uuid = cookie_array[i].substr(key.length, cookie_array[i].length);
+	    break;
+	}
+	i++;
+    }
+    console.log(uuid);
     
     body = JSON.stringify({
-	title: document.title
+	title: document.title,
+	uuid: uuid
     });
 
     req = new XMLHttpRequest();
-    req.open("POST", url, false);
+    req.open("POST", 'http://pr.aiit.ac.jp/pv', false);
     req.setRequestHeader('Content-Type', 'application/json');
     req.setRequestHeader('Accept', 'application/json');
 
