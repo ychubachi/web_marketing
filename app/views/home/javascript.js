cef04edc1323732ae6c1727180a04c23eadc4327
Yoshihide Chubachi <yoshi@chubachi.net>
2012-07-03 14:59:08 +0900

    use async mode for js

--- app/views/home/javascript.js
+++ app/views/home/javascript.js
@@ -1,33 +1,25 @@
 function sendPostRequest() {
-    uuid = "";
-    key = "web_marketing_uuid=";
-    i = 0;
-    cookie_array = document.cookie.split("; ");
-    while(cookie_array[i]) {
-	console.log(cookie_array[i]);
-	if(cookie_array[i].substr(0, key.length) == key) {
-	    uuid = cookie_array[i].substr(key.length, cookie_array[i].length);
-	    break;
-	}
-	i++;
-    }
-    console.log(uuid);
-    
-    body = JSON.stringify({
-	title: document.title,
-	uuid: uuid
-    });
-
     req = new XMLHttpRequest();
     req.open("POST", 'http://pr.aiit.ac.jp/pv', true);
     req.setRequestHeader('Content-Type', 'application/json');
     req.setRequestHeader('Accept', 'application/json');
     req.withCredentials = true;
+    req.onreadystatechange = function(event) {
+	if(req.readyState === 4) {
+	    if(req.status === 200) {
+		console.log('status code : ' + request.status);
+		console.log('response text : ' + request.responseText);
+	    } else {
+		console.log("Error", req.statusText);
+	    }
+	}
+    };
 
+    body = JSON.stringify({
+	title: document.title,
+    });
     req.send(body);
     return req;
 }
 
 request = sendPostRequest();
-console.log('status code : ' + request.status);
-console.log('response text : ' + request.responseText);
