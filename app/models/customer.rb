1a9a0bc1ccc12e98ca63691ef70c1294cef1a375
Yoshihide Chubachi <yoshi@chubachi.net>
2012-07-07 23:45:57 +0900

    add to_label to customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < ActiveRecord::Base
   attr_accessible :address, :browser_id, :inquiry, :email, :family_name, :given_name, :postal_code
   belongs_to :browser
+
+  def to_label
+    "#{family_name} #{given_name}"
+  end
 end
