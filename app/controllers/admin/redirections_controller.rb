5a8c841a62e66079bb6a7b8ac6550652d499cfb4
Yoshihide Chubachi <yoshi@chubachi.net>
2012-07-12 23:20:44 +0900

    add medium and target to redirections

--- app/controllers/admin/redirections_controller.rb
+++ app/controllers/admin/redirections_controller.rb
@@ -2,7 +2,7 @@ class Admin::RedirectionsController < ApplicationController
   before_filter :authenticate_user!
 
   active_scaffold :redirection do |conf|
-    conf.columns = [:code, :title, :is_default, :medium, :target, :requests, :created_at, :updated_at]
-    conf.list.sorting = {code: 'ASC'}
+    conf.columns = [:code, :is_default, :medium, :target, :requests, :created_at, :updated_at]
+    conf.list.sorting = {code: 'DESC'}
   end
 end 
