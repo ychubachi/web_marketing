9d7f6d04b95146ce68fc219ed020419029f9e532
Yoshihide Chubachi <yoshi@chubachi.net>
2012-07-27 10:34:57 +0900

    refactoring

--- app/controllers/landing/page_controller.rb
+++ app/controllers/landing/page_controller.rb
@@ -8,41 +8,55 @@ class Landing::PageController < ApplicationController
     @guidance_table = [['7/28(土)','15:00～16:30'],['8/ 8(水)','19:30～21:00'],['8/17(金)','19:30～21:00']]
   end
 
+  def image
+    begin
+      file_base_name = params[:file]
+      image_file_path = "app/views/landing/page/images/#{file_base_name}.gif"
+      send_file(image_file_path, :disposition => 'inline')
+    rescue => e
+      logger.error 'error on reading image file'
+      puts e
+      redirect_to '/lp/sorry'
+    end
+  end
+
   def create
     begin
-      # save a new customer information.
+      # get this browser.
       browser = get_browser
+
+      # save a new customer information.
       customer = Customer.new(params[:customer])
       customer.browser = browser
       customer.save!
-
-      # record a conversion.
+      
+      # record a conversion with a new request.
       conversion = Conversion.where('title = :title', {title: "資料請求"}).first_or_initialize
       if conversion.new_record? then
         conversion.title = "資料請求"
         conversion.save!
       end
+      save_request!(browser, conversion)
 
-      # save a new request.
-      my_request = Request.new
-      my_request.referrer = request.referer.to_s
-      my_request.action = conversion
-      my_request.browser = browser
-      my_request.save!
-
-      # obtain the convrsion path of the browser.
-      # conversion_path = Request.where("browser_id = :browser_id", {browser_id: browser}).order("created_at ASC")
+      # send an email with a conversion path.
       conversion_path = browser.requests.order("created_at ASC")
-      logger.debug (conversion_path.to_yaml)
-
-      # send an email.
       ConversionMailer.conversion(customer, conversion_path).deliver
 
+      # redirection
       redirect_to '/lp/thank_you'
-      logger.info 'successfully created.'
-    rescue
+    rescue => e
+      logger.error 'error on saving data'
+      puts e
       redirect_to '/lp/sorry'
-      logger.info 'error on saving data'
     end
+  end
+
+  def save_request!(browser, action)
+    # save a new request.
+    my_request = Request.new
+    my_request.referrer = request.referer.to_s
+    my_request.action = action
+    my_request.browser = browser
+    my_request.save!
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,14 @@ WebMarketing::Application.routes.draw do
   match '/pv'       => 'home#page'
   match '/tracker'  => 'home#javascript'
   match '/admin'    => 'admin/users#index'
-  match '/lp'           => 'landing/page#index'
-  match '/lp/thank_you' => 'landing/page#thank_you'
-  match '/lp/sorry'     => 'landing/page#sorry'
-  match '/lp/pm'        => 'landing/page#pm'
+  match '/lp'            => 'landing/page#index'
+  match '/lp/thank_you'   => 'landing/page#thank_you'
+  match '/lp/sorry'       => 'landing/page#sorry'
+  match '/lp/pm'          => 'landing/page#pm'
+  match '/lp/image/:file' => 'landing/page#image'
 
   namespace :landing do
-    resources :page, :only => [:index, :create]
-    get 'page/thank_you'
-    get 'page/sorry'
+    resources :page, :only => [:create]
   end
 
   namespace :admin do
@@ -30,14 +29,6 @@ WebMarketing::Application.routes.draw do
     resources :targets do as_routes end
     resources :media do as_routes end
     resources :browsers do as_routes end
-    # followings will be deleted?
-    # resources :pages
-    # resources :requests
-    # resources :actions
-    # resources :redirections
-    # resources :targets
-    # resources :media
-    # resources :browsers
     resources :users, :only => [:show, :index]
   end
   
