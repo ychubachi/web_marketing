17e62c7939ed9ee1541b395f6c03107870f95667
Yoshihide Chubachi <yoshi@chubachi.net>
2012-08-02 10:02:07 +0900

    error on loading html5shiv

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
     # lookup the browser from DB
     browser = Browser.where('uuid = :uuid', {uuid: uuid}).first_or_initialize
     if browser.new_record?
-      logger.info '### create a new browser and save it.'
+      logger.debug '### create a new browser and save it.'
       browser.uuid = uuid
       browser.user_agent = request.user_agent.to_s
       browser.save
