e43b547124de7f151027408dfa89051a21605241
Yoshihide Chubachi <yoshi@chubachi.net>
2013-01-21 00:40:50 +0900

    apt-get版からrvm版にすることで，webratを追加しても問題なくなりました．

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -61,3 +61,13 @@ end
 # - These instructions should self-destruct in 10 seconds.  If they don't,
 #   feel free to delete them.
 #
+
+# ------------------------------------------------------------------------
+
+require "webrat"
+
+Webrat.configure do |config|
+  config.mode = :rails
+end
+
+
