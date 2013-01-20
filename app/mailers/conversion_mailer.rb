a676cfaf2afeba42676b272d4aebba02d987b857
Yoshihide Chubachi <yoshi@chubachi.net>
2013-01-20 20:29:42 +0900

    customer.inquiryが空の場合の処理を追加しました

--- app/mailers/conversion_mailer.rb
+++ app/mailers/conversion_mailer.rb
@@ -4,6 +4,10 @@ class ConversionMailer < ActionMailer::Base
 
   def conversion(customer, conversion_path)
     @customer = customer
+    # customer.inquiryはJSで生成するのでテスト時は空です
+    if customer.inquiry == "" then
+      customer.inquiry = "[]"
+    end
     @inquiry = ActiveSupport::JSON.decode(customer.inquiry)
     @conversion_path = conversion_path
     mail(
--- features/form/email.feature
+++
@@ -1,7 +0,0 @@
-Feature: email
-  In order to...
-
-  Scenario: ユーザが資料請求をする
-    Given a clear email queue
-    When I submit my address
-    Then they should receive an email
